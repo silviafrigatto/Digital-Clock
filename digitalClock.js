aa0391ef510fba1b6fa7f7c6af0f255c34edfb00
Silvia Frigatto <silvia.frigatto@gmail.com>
2022-02-15 19:21:15 -0300

    Fixed the hours variable.

--- digitalClock.js
+++ digitalClock.js
@@ -1,20 +1,22 @@
-const Mytime = ()=> {
+const Mytime =() => {
 	let currentDate = new Date();
 	let hours = currentDate.getHours();
 	let minutes = currentDate.getMinutes();
 	let seconds = currentDate.getSeconds();
 	let session = "AM";
 	
-	if(hour == 0){
-		hour = 12;
+	if(hours == 0){
+		hours = 12;
 	}
 	
-	if(hour > 12){
-		hour = hour - 12;
+	if(hours > 12){
+		hours = hours - 12;
 		session = "PM";
 	}
 	
 	let time = hours + ":" + minutes + ":" + seconds + " " + session;
 	
-	document.getElementById("showtime").innerHTML = time;
+	document.getElementById("showTime").innerHTML = time;
 }
+
+Mytime();
