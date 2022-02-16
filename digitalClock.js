7568d33f644755bffc81abe1150af2db4cbfa2dd
Silvia Frigatto <silvia.frigatto@gmail.com>
2022-02-15 21:03:59 -0300

    Adjusted minutes

--- digitalClock.js
+++ digitalClock.js
@@ -16,6 +16,8 @@ const Mytime =() => {
 	
 	hours = (hours < 10) ? "0" + hours : hours;
 	
+	minutes = (minutes < 10) ? "0" + minutes : minutes;
+	
 	seconds= (seconds < 10) ? "0" + seconds : seconds;
 	
 	let time = hours + ":" + minutes + ":" + seconds + " " + session;
